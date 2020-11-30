165df681f2e0baf2c0f34270ca9717d7b4a92996
dougie-hawes <dougiehawes@hotmail.com>
2020-11-30 16:26:49 +0000

    redux implemented

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
+
+import { useSelector, useDispatch } from "react-redux";
 
 import "./style.min.css";
 
@@ -8,24 +9,17 @@ import Product from "./Product";
 import MessageBox from "./MessageBox";
 import LoadingBox from "./LoadingBox";
 
+import { listProducts } from "../actions/productActions.js";
+
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        const { data } = await axios.get("/api/products");
-        setLoading(false);
-        setProducts(data);
-      } catch (error) {
-        setError(error.message);
-        setLoading(false);
-      }
-    };
-    fetchData();
+    dispatch(listProducts());
   }, []);
 
   return (
@@ -36,7 +30,6 @@ const HomeScreen = () => {
         <MessageBox variant="error">{error}</MessageBox>
       ) : (
         <div className="home-screen">
-          {" "}
           {products.map((item) => (
             <Product key={item._id} product={item} />
           ))}
--- client/src/components/ProductScreen.js
+++ client/src/components/ProductScreen.js
@@ -1,41 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
 import "./style.min.css";
 
-import data from "../data.js";
+import LoadingBox from "./LoadingBox";
+import MessageBox from "./MessageBox";
 
 import Rating from "./Rating.js";
 
-const ProductScreen = (props) => {
-  const product = data.products.find((x) => x._id === props.match.params.id);
+import { detailsProduct } from "../actions/productActions";
 
-  if (!product) {
-    return <div>product not found</div>;
-  }
+const ProductScreen = (props) => {
+  const dispatch = useDispatch();
+  const productId = props.match.params.id;
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    dispatch(detailsProduct(productId));
+  }, [dispatch, productId]);
 
   return (
-    <div className="product-screen">
-      <div className="column">
-        <Link to="/">return to home screen</Link>
-        <br />
-        <img src={product.image} alt={product.name} />
-      </div>
-      <div className="column">
-        <h3 className="product-screen-title">{product.name}</h3>
-        <Rating rating={product.rating} />
-        <div className="product-screen-price">£ {product.price}</div>
-        <div className="product-screen-description">{product.description}</div>
-      </div>
-      <div className="column">
-        <div className="purchase-details-box">
-          <div className="product-screen-price">£ {product.price}</div>
-          {product.qtyStock === 0 ? <>SOLD-OUT</> : <>IN-STOCK</>}
-          <button className="purchase-details-box-button">Add To Cart</button>
+    <>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="error">{error}</MessageBox>
+      ) : (
+        <div className="product-screen">
+          <div className="column">
+            <Link to="/">return to home screen</Link>
+            <br />
+            <img src={product.image} alt={product.name} />
+          </div>
+          <div className="column">
+            <h3 className="product-screen-title">{product.name}</h3>
+            <Rating rating={product.rating} />
+            <div className="product-screen-price">£ {product.price}</div>
+            <div className="product-screen-description">
+              {product.description}
+            </div>
+          </div>
+          <div className="column">
+            <div className="purchase-details-box">
+              <div className="product-screen-price">£ {product.price}</div>
+              {product.qtyStock === 0 ? <>SOLD-OUT</> : <>IN-STOCK</>}
+              <button className="purchase-details-box-button">
+                Add To Cart
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
